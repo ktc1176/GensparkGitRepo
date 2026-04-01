5bca2121f6d31831d96b2869bc0b6e28bfe7fa7f
ktc1176 <ktc1176@gmail.com>
2026-04-01 15:11:14 -0500

    -test

--- GitPracticeProject/src/main/java/Main.java
+++ GitPracticeProject/src/main/java/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public void main(String[] args){
-        return;
+        Calculator calculator = new Calculator();
+        calculator.setNum1(10);
+        calculator.setNum2(20);
+        System.out.println("Sum: " + calculator.add());
+        System.out.println("Difference: " + calculator.subtract());
     }
 }
