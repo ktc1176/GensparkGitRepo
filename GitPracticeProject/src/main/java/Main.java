c0a2556ad1f7f00ccd2540638012773f3da9ce9a
ktc1176 <ktc1176@gmail.com>
2026-04-01 15:17:58 -0500

    -Calculator test case in main class

--- GitPracticeProject/src/main/java/Main.java
+++ GitPracticeProject/src/main/java/Main.java
@@ -1,8 +1,13 @@
 public class Main {
     public void main(String[] args){
+        //create new calculator class
         Calculator calculator = new Calculator();
+
+        //set input numbers
         calculator.setNum1(10);
         calculator.setNum2(20);
+
+        //output results to console
         System.out.println("Sum: " + calculator.add());
         System.out.println("Difference: " + calculator.subtract());
     }
