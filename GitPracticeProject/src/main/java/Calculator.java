8c4285c8e1a19ba314f6ba582d7d6f1ac0003e5b
ktc1176 <ktc1176@gmail.com>
2026-04-01 14:51:57 -0500

    -added comments to calculator class -created getters and setter functions for private variables

--- GitPracticeProject/src/main/java/Calculator.java
+++ GitPracticeProject/src/main/java/Calculator.java
@@ -12,10 +12,28 @@ public class Calculator {
         this.num2 = 0;
     }
 
+    public int getNum1() {
+        return num1;
+    }
+
+    public void setNum1(int num1) {
+        this.num1 = num1;
+    }
+
+    public int getNum2() {
+        return num2;
+    }
+
+    public void setNum2(int num2) {
+        this.num2 = num2;
+    }
+
+    //add function
     public int add(){
         return num1 + num2;
     }
 
+    //subtract function
     public int subtract(){
         return num1 - num2;
     }
